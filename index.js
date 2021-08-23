31f7623f06337ef50ab4f77fcc4fe2203bae9c29
Yukimi Kazari <dev@9854.moe>
2021-08-23 19:24:57 -0400

    Added new options

--- index.js
+++ index.js
@@ -132,7 +132,19 @@ async function getImage(opts, extra) {
                 if (extra && extra.incimentalFileNames) { indexCount++ };
                 console.log(`${json.randomImagev2[item].eid} - ${json.randomImagev2[item].pinned} - ${json.randomImagev2[item].serverName.toUpperCase()}:/${json.randomImagev2[item].className}/${json.randomImagev2[item].channelName} - ${json.randomImagev2[item].date}`);
                 try {
-                    const response = await got(json.randomImagev2[item].fullImage, {cookieJar, dnsLookupIpVersion: 'ipv4'});
+                    const response = await got(json.randomImagev2[item].fullImage, { headers: {
+                            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                            'accept-language': 'en-US,en;q=0.9',
+                            'cache-control': 'max-age=0',
+                            'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Microsoft Edge";v="92"',
+                            'sec-ch-ua-mobile': '?0',
+                            'sec-fetch-dest': 'document',
+                            'sec-fetch-mode': 'navigate',
+                            'sec-fetch-site': 'none',
+                            'sec-fetch-user': '?1',
+                            'upgrade-insecure-requests': '1',
+                            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.73'
+                        }, cookieJar, dnsLookupIpVersion: 'ipv4'});
                     if (response.body) {
                         let fileExt = 'jpg';
                         const _wallpaperPath = path.join(path.resolve(wallpaperLocation), (extra && extra.path) ? extra.path : '', `./ads-wallpaper_${(indexCount > 0) ? 'index' + indexCount: json.randomImagev2[item].eid}.${fileExt}`)
@@ -232,7 +244,7 @@ async function getWebCapture(opts, filename, extra) {
 
             const cookies = cookieJar.getCookiesSync(baseURL).map(c => c.toString());
             const pageRequest = new pageres({
-                delay: 1,
+                delay: 5,
                 timeout: 15,
                 cookies: cookies,
                 filename: _filename
